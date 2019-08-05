0b5509f608e1bcf4d3e65ed2c40ee107969d85de
Mona-S <forward2cm@gmail.com>
2019-08-05 19:20:41 +0000

    initial files

---
+++ calc.js
@@ -0,0 +1,31 @@
+//console.log("Hello, World");
+
+function doMath(num1, operator, num2){
+    num1 = parseFloat(num1);
+    num2 = parseFloat(num2);
+    if(operator === "+"){
+        return num1 + num2;
+    }
+    else if(operator === "-"){
+        return num1 - num2;
+    }
+    else if(operator === "*"){
+        return num1 * num2;
+
+    }else if(operator === "/"){
+        return num1 /num2;
+    }
+}
+
+var num1 = process.argv[2];
+var num2 = process.argv[4];
+var operator = process.argv[3];
+
+console.log(doMath(num1, operator, num2));
+
+// var answer = doMath(2, "+", 4);
+// console.log(answer);
+
+//console.log(process);
+//console.log(process.argv);
+
